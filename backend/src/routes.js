3539e513d30a9b59941b059952e103b3abb21118
Gabriel Bur <gabriel.bur@outlook.com>
2019-10-02 22:31:31 -0300

    Api finished,  booking and dashboard controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,13 +4,22 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
-
+const DashBoardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
-
+//sessions
 routes.post('/api/sessions', SessionController.store);
-routes.post('/api/spots', upload.single('thumbnail') ,SpotController.store);
 
+//spots
+routes.post('/api/spots', upload.single('thumbnail') ,SpotController.store);
+routes.get('/api/spots', SpotController.index);
+
+//dashboard
+routes.get('/api/dashboards', DashBoardController.show);
+
+//booking
+routes.post('/api/spots/:spot_id/bookings',BookingController.store);
 module.exports = routes;
