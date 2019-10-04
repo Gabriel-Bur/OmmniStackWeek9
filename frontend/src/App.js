b3a624ebccd961c62d28e63affa1d34c358e0cbf
Gabriel Bur <gabriel.bur@outlook.com>
2019-10-04 00:11:11 -0300

    updates in frontend and minor fixes on api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 import './App.css';
+
 import logo from './assets/logo.svg';
-import api from './services/api';
+
+import Routes from './routes';
 
 
 function App() {
-  const [email, setEmail] = useState('');
-
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    
-    const response = await api.post("sessions", { email })
-    
-    const { id } = response.data;
-
-    localStorage.setItem("user", id);
-
-  }
-
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
       <div className="content">
-        <p> 
-          Lugares para programadores e talentos.
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail*</label>
-          <input 
-            type="email" 
-            id="email"
-            required
-            placeholder="Digite seu e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)} />
-            
-            <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
