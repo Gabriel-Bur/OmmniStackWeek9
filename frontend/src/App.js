bbf01d7f490348dd06f275dd9d09246d37e7a288
Gabriel Sampaio Bur <gabriel.bur@radixeng.com.br>
2019-10-03 21:54:02 -0300

    changes in front and back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import api from './services/api';
 
 
 function App() {
+  const [email, setEmail] = useState('');
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const response = await api.post("sessions", { email })
+    
+    const { id } = response.data;
+
+    localStorage.setItem("user", id);
+
+  }
+
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
       <div className="content">
-        <p>
+        <p> 
           Lugares para programadores e talentos.
         </p>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail*</label>
           <input 
             type="email" 
             id="email"
+            required
             placeholder="Digite seu e-mail"
-          />
-            <button type="button">Entrar</button>
+            value={email}
+            onChange={event => setEmail(event.target.value)} />
+            
+            <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
     </div>
